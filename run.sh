6c34737772b1c678fe1d81d02d903cdaf7e6be50
adiluck <adiluckyo@gmail.com>
2019-06-29 09:09:49 +0700

    merge docker-compose file

--- run.sh
+++ run.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 echo "-------------------------Create Docker network-------------------------"
 docker network create --subnet=10.11.1.0/24 TDTest
-echo "-------------------------Install Ldap service-------------------------"
-cd ldap
-docker-compose up -d
-echo "-------------------------Install GitLab-------------------------"
-cd ..
-cd gitlab
+echo "-------------------------Install services-------------------------"
 docker-compose up -d
 echo "-------------------------Add devops user-------------------------"
 docker exec -it ldap_service bash ./autoadduser.sh
